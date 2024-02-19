234533ea0de09361925cdeb492b7c5ba563f94d7
tinvidovic <tin@vidovic.family>
2024-02-19 16:29:52 +0100

    Fix SelectableItem
    Fixed SelectableItem spacing, added divider and added alpha for disabled parameters

--- app/src/main/java/com/nticoding/electroluxassignment/presentation/components/SelectableItem.kt
+++ app/src/main/java/com/nticoding/electroluxassignment/presentation/components/SelectableItem.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -27,6 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nticoding.electroluxassignment.R
 import com.nticoding.electroluxassignment.domain.model.SelectableOption
+import com.nticoding.electroluxassignment.ui.theme.DisabledAlpha
 import com.nticoding.electroluxassignment.ui.theme.ElectroluxAssignmentTheme
 import com.nticoding.electroluxassignment.util.UIText
 
@@ -37,20 +39,26 @@ import com.nticoding.electroluxassignment.util.UIText
  * @param modifier the [Modifier] to be applied to this selectable item.
  * @param shape shape of this selectable item.
  * @param color color of this selectable item.
+ * @param enabled When disabled has 0.4f alpha and is not clickable
  */
 @Composable
 fun SelectableItem(
     selectableOption: SelectableOption,
-    onSelected: () -> Unit,
+    onSelected: (SelectableOption) -> Unit,
     modifier: Modifier = Modifier,
     shape: CornerBasedShape = ElectroluxAssignmentTheme.shapes.small,
-    color: Color = ElectroluxAssignmentTheme.colorScheme.container
+    color: Color = ElectroluxAssignmentTheme.colorScheme.container,
+    enabled: Boolean = true
 ) {
+
+    val alphaModifier = modifier
+        .alpha(if (enabled) 1f else DisabledAlpha)
+
     Surface(
-        onClick = { onSelected() }, // Make the entire surface clickable
+        onClick = { if (enabled) onSelected(selectableOption) }, // Make the entire surface clickable
         shape = shape,
         color = color,
-        modifier = modifier
+        modifier = alphaModifier
             .semantics(
                 // Treat descendants as a single component
                 mergeDescendants = true,
@@ -63,52 +71,14 @@ fun SelectableItem(
 
         val context = LocalContext.current
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-
-            // Leading Icon
-            Icon(
-                painter = painterResource(id = selectableOption.iconResId),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(
-                        horizontal = 16.dp,
-                        vertical = 16.dp
-                    )
-            )
-
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(
-                        vertical = 16.dp
-                    )
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                // Title text
-                Text(
-                    text = selectableOption.title.asString(context),
-                    style = ElectroluxAssignmentTheme.typography.title1,
-                    textAlign = TextAlign.Start,
-                    color = ElectroluxAssignmentTheme.colorScheme.contentPrimary
-                )
 
-                // Description text (visible when selected)
-                // Simple visibility animation, to show/hide description if selected
-                AnimatedVisibility(visible = selectableOption.selected) {
-                    Text(
-                        text = selectableOption.description.asString(context),
-                        style = ElectroluxAssignmentTheme.typography.body1,
-                        textAlign = TextAlign.Start,
-                        color = ElectroluxAssignmentTheme.colorScheme.contentSecondary,
-                    )
-                }
-            }
-
-            // Display checked icon if selected
-            if (selectableOption.selected) {
+                // Leading Icon
                 Icon(
-                    painter = painterResource(id = R.drawable.ic_check),
+                    painter = painterResource(id = selectableOption.iconResId),
                     contentDescription = null,
                     modifier = Modifier
                         .padding(
@@ -116,15 +86,65 @@ fun SelectableItem(
                             vertical = 16.dp
                         )
                 )
+
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(
+                            vertical = 16.dp
+                        )
+                ) {
+                    // Title text
+                    Text(
+                        text = selectableOption.title.asString(context),
+                        style = ElectroluxAssignmentTheme.typography.title1,
+                        textAlign = TextAlign.Start,
+                        color = ElectroluxAssignmentTheme.colorScheme.contentPrimary
+                    )
+
+                    Spacer(modifier = Modifier.height(4.dp))
+                    // Description text (visible when selected)
+                    // Simple visibility animation, to show/hide description if selected
+                    AnimatedVisibility(visible = selectableOption.selected) {
+                        Text(
+                            text = selectableOption.description.asString(context),
+                            style = ElectroluxAssignmentTheme.typography.body1,
+                            textAlign = TextAlign.Start,
+                            color = ElectroluxAssignmentTheme.colorScheme.contentSecondary,
+                        )
+                    }
+                }
+
+                // Display checked icon if selected
+                if (selectableOption.selected) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_check),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(
+                                horizontal = 16.dp,
+                                vertical = 16.dp
+                            )
+                    )
+                }
             }
+
+            HorizontalDivider(
+                thickness = 1.dp,
+                color = ElectroluxAssignmentTheme.colorScheme.divider,
+                modifier = Modifier.padding(
+                    horizontal = 16.dp
+                )
+            )
         }
+
     }
 }
 
 @Preview(name = "Light Mode")
 @Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun SelectableItemPreview() {
+fun EnabledSelectableItemPreview() {
 
     val selectableOption = SelectableOption(
         UIText.DynamicString("Cotton Eco"),
@@ -136,6 +156,26 @@ fun SelectableItemPreview() {
         SelectableItem(
             selectableOption = selectableOption,
             onSelected = { /*TODO*/ },
+        )
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun DisabledSelectableItemPreview() {
+
+    val selectableOption = SelectableOption(
+        UIText.DynamicString("Cotton Eco"),
+        UIText.DynamicString("Cupboard-dries cottons with maximum energy saving"),
+        R.drawable.ic_faq
+    )
+
+    ElectroluxAssignmentTheme {
+        SelectableItem(
+            selectableOption = selectableOption,
+            onSelected = { /*TODO*/ },
+            enabled = false
         )
     }
 }
